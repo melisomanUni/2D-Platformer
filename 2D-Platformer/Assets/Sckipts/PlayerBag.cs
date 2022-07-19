3a8b1e1b228b95e0eb95732e1a9c85d76f2a234a
Илюша <ilya_b_2013@mail.ru>
2022-07-19 16:30:02 +0300

    Make count coins

--- 2D-Platformer/Assets/Sckipts/PlayerBag.cs
+++ 2D-Platformer/Assets/Sckipts/PlayerBag.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
 
 public class PlayerBag : MonoBehaviour
 {
     private int _countCoint;
+
+    public event UnityAction<int> CoinCollected;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.TryGetComponent(out Coin coin))
         {
             _countCoint++;
+            CoinCollected?.Invoke(_countCoint);
             Destroy(collision.gameObject);
         }
     }
