ac29a77c6c44aad0b7c8011b9d511e1a8a3b2861
Илюша <ilya_b_2013@mail.ru>
2022-07-19 16:52:58 +0300

    additional load

---
+++ 2D-Platformer/Assets/Sckipts/Test.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+
+public class Test : MonoBehaviour
+{
+    [SerializeField] TMP_Text _score;
+    [SerializeField] private PlayerBag _playerBag;
+
+    private void OnEnable()
+    {
+        _playerBag.CoinCollected += OnCoinCollected;
+    }
+
+    private void OnDisable()
+    {
+        _playerBag.CoinCollected -= OnCoinCollected;
+    }
+
+    private void OnCoinCollected(int coinCount)
+    {
+        _score.text = coinCount.ToString();
+    }
+}
